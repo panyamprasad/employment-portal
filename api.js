9b6989b380f40fe4c0fb57869ec45517649b2ecb
Prasad <37583509+PrasadPanyam@users.noreply.github.com>
2023-10-09 16:13:07 +0530

    Intial Setup

--- api.js
+++ api.js
@@ -1,10 +1,30 @@
 const { DynamoDB } = require('aws-sdk');
 const dynamoDb = new DynamoDB.DocumentClient();
 
-module.exports.employeePortal = async function(event){
+module.exports.employeeExperience = async function(event){
     console.log('Request Event:', event);
     let response;
-    // switch(true){
-    //     case event.httpMethod === 'GET':
-    // }
+    switch(true){
+        case event.httpMethod === 'GETALL':
+            response = await getAllEmployeesExperienceInfo();
+            console.log(response);
+            break;
+        case event.httpMethod === 'GET':
+            response = await getEmployeeExperienceInfo();
+            console.log(response);
+            break;
+        case event.httpMethod === 'POST':
+            response = await saveExperienceInfo();
+            console.log(response);
+            break;
+        case event.httpMethod === 'PUT':
+            response = await updateExperienceInfo();
+            console.log(response);
+            break;
+        default:
+            response = buildResponse(404, '404 Not Found'); 
+    }
+    return response;
+
+
 }
