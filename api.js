c54dfa2480d86262a4e038783af7563b91bb02dd
Prasad <37583509+PrasadPanyam@users.noreply.github.com>
2023-10-10 01:10:43 +0530

    Created Lambda functions for create, get & getAll records.

--- api.js
+++ api.js
@@ -25,109 +25,144 @@ module.exports.employeeExperience = async function (event) {
   // }
   // return response;
   const httpMethod = event.httpMethod;
-  switch (httpMethod) {
-    case "POST":
-      return saveExperienceInfo(event);
-    case "PUT":
-      return updateExperience(event);
-    case "GET":
-      if (event.pathParameters && event.pathParameters.employeeId) {
-        return getEmployeeExperience(event);
-      } else {
-        return getAllEmployeesExperience(event);
-      }
-    default:
-      return {
-        statusCode: 405,
-        body: JSON.stringify({ message: "Method not allowed" }),
-      };
-  }
+    switch (httpMethod) {
+      case "POST":
+        return saveExperienceInfo(event);
+      case "PUT":
+        return updateExperience(event);
+      case "GET":
+        if (event.pathParameters && event.pathParameters.employeeId) {
+          return getEmployeeExperience(event);
+        } else {
+          return getAllEmployeesExperience(event);
+        }
+      default:
+        return {
+          statusCode: 405,
+          body: JSON.stringify({ message: "Method not allowed" }),
+        };
+    }
 
   //Save Record
-  async function saveExperienceInfo(event) {
-    try {
-      const requestBody = JSON.parse(event.body);
+    async function saveExperienceInfo(event) {
+        try {
+        const requestBody = JSON.parse(event.body);
 
-      // Validate StartDate and EndDate
-      if (new Date(requestBody.StartDate) >= new Date(requestBody.EndDate)) {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({ error: "EndDate must be after StartDate" }),
+        // Validate StartDate and EndDate
+        if (new Date(requestBody.StartDate) >= new Date(requestBody.EndDate)) {
+            return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "EndDate must be after StartDate" }),
+            };
+        }
+        const params = {
+            TableName: process.env.EMPLOYEE_TABLE,
+            Item: requestBody,
         };
-      }
-      const params = {
-        TableName: process.env.EMPLOYEE_TABLE,
-        Item: requestBody,
-      };
-      await dynamoDb.put(params).promise();
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "Experience info added successfully...!",
-        }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "Internal Server Error...!",
-        }),
-      };
-    }
-  }
+        await dynamoDb.put(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+            message: "Experience info added successfully...!",
+            }),
+        };
+        } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+            message: "Internal Server Error...!",
+            }),
+        };
+        }
+    };
+
+    //Update Record
+    async function updateExperience(event){
+        try{
+            const requestBody = JSON.parse(event.body);
+            const updateExpression = 'SET experience = :experience';
+            const experienceAttributeValues = {
+                ':experience' : requestBody.experience
+            };
+            const params = {
+              TableName: process.env.EMPLOYEE_TABLE,
+              Key: {
+                EmpId: event.pathParameters.employeeId
+              },
+              updateExpression: updateExpression,
+              experienceAttributeValues: experienceAttributeValues,
+              ReturnValues: 'UPDATED_NEW'
+            };
+            const result = await dynamoDB.update(params).promise();
+            return {
+              statusCode: 200,
+              body: JSON.stringify({
+                message: "Record Updated Successfully...!"
+              }),
+            };
+        }catch(error){
+          return{
+            statusCode: 500,
+            body: JSON.stringify({
+              message: 'Internal server error...!'
+            })
+          };
+        }
+    };
 
   //Get Record
-  async function getEmployeeExperience(event) {
-    try {
-      const params = {
-        TableName: process.env.EMPLOYEE_TABLE,
-        Key: {
-          EmpId: event.pathParameters.employeeId,
-        },
-      };
-      const result = await dynamoDb.get(params).promise();
-      if (!result.Item) {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({ message: "Record not found...!" }),
+    async function getEmployeeExperience(event) {
+        try {
+        const params = {
+            TableName: process.env.EMPLOYEE_TABLE,
+            Key: {
+            EmpId: event.pathParameters.employeeId,
+            },
         };
-      }
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result.Item),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "Internal server error...!",
-        }),
-      };
+        const result = await dynamoDb.get(params).promise();
+        if (!result.Item) {
+            return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Record not found...!" }),
+            };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+        } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+            message: "Internal server error...!",
+            }),
+        };
+        }
     }
-  }
+
   //Get All Records
-  async function getAllEmployeesExperience(event) {
-    try {
-      const params = {
-        TableName: process.env.EMPLOYEE_TABLE,
-      };
-      const result = await dynamoDb.scan(params).promise();
-      if (!result.Items || result.Items.length === 0) {
-        return {
-          statusCode: 404,
-          body: JSON.stringify({ message: "No records found...!" }),
+    async function getAllEmployeesExperience(event) {
+        try {
+        const params = {
+            TableName: process.env.EMPLOYEE_TABLE,
         };
-      }
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result.Items),
-      };
-    } catch (error) {
-      console.error("Error fetching all employees experience:", error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Internal server error" }),
-      };
+        const result = await dynamoDb.scan(params).promise();
+        if (!result.Items || result.Items.length === 0) {
+            return {
+            statusCode: 404,
+            body: JSON.stringify({ message: "No records found...!" }),
+            };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Items),
+        };
+        } catch (error) {
+        console.error("Error fetching all employees experience:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server error" }),
+        };
+        }
     }
-  }
 };
